24fb590b7397f3f0b304f59c1be0503452ad936a
fedecame <fede.cameriere@gmail.com>
2020-11-25 23:27:28 -0300

    comento cors que rompe

--- src/main/java/unq/edu/tpi/desapp/BackendDesappApiApplication.java
+++ src/main/java/unq/edu/tpi/desapp/BackendDesappApiApplication.java
@@ -25,7 +25,7 @@ public class BackendDesappApiApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-                registry.addMapping("/**").allowedOrigins("http://localhost:8090");
+//                registry.addMapping("/**").allowedOrigins("http://localhost:8090");
             }
         };
     }
